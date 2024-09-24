20735277e5753a7bb7842d4dad3d102ffc242cca
mizzpika <148228003+mizzpika@users.noreply.github.com>
2024-09-24 21:05:34 +0900

    Update template.hpp

--- Template/template.hpp
+++ Template/template.hpp
@@ -1,4 +1,4 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 #pragma GCC optimize("Ofast,unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
 using namespace std;
