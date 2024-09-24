b267ff98a9874cd2b30522ca54bbf06a245c3040
mizzpika <148228003+mizzpika@users.noreply.github.com>
2024-09-24 20:59:57 +0900

    Create template.hpp

---
+++ Template/template.hpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(ll i = 0LL; i < (ll)n; ++i)
+#define rep1(i,n) for(ll i = 1LL; i <= (ll)n; ++i)
+#define rep2(i,m,n) for(ll i = (ll)m; i < (ll)n; ++i)
+#define rrep(i,n) for(ll i = (ll)n - 1; i >= 0LL; --i)
+#define rrep1(i,n) for(ll i = (ll)n; i > 0LL; --i)
+#define rrep2(i,m,n) for(ll i = (ll)m; i > (ll)n; --i)
+#define _GLIBCXX_DEBUG
+#define _CRT_SECURE_NO_WARNINGS
